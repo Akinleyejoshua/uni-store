13abb49076b6d5f08a1d9e0eeac8f0a28365ea7e
Joshua <akinleyejoshua.dev@gmail.com>
2023-03-03 19:49:44 +0100

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -57,7 +57,7 @@ export default function Checkout() {
         reference: (new Date()).getTime(),
         email: email,
         amount: totalExchange,
-        publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PK,
+        publicKey: "pk_test_5b50c65241250e7c803b56c2d35668186e4eb5b2",
     };
 
     const handlePaystackSuccessAction = (reference) => {
